3ded85626148ac95770e00d60f64dc46a7a4779a
wizking-beep <barakabrian003@gmail.com>
2025-02-20 22:22:44 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+echo json_encode(["message" => "CORS is working on Render!"]);
+?>
